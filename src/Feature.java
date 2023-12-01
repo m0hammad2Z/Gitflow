1134955192e8a174ade39298ee308e9414af5f3f
Mohammad Al-Zaro <imhamd33@gmail.com>
2023-12-01 16:17:25 +0300

    Project init

---
+++ src/Feature.java
@@ -0,0 +1,13 @@
+public class Feature {
+
+    private String name;
+    public Feature(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Feature feature= new Feature("Feature");
+        System.out.println(feature.getName());
+
+
+    }
+}
